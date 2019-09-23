1aaa74aa2b63315c707c0ca9f5fd9d22e3f1e5ac
mason <ab300819@163.com>
2019-09-24 01:32:47 +0800

    添加 make_socket 函数

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main() {
     printf("Hello, World!\n");
     return 0;
+}
+
+int make_socket(uint16_t port) {
+    int sock;
+    struct sockaddr_in name;
+
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    name.sin_family = AF_INET;
+    name.sin_port = htons(port);
+    name.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sock, (struct sockaddr *) &name, sizeof(name)) < 0) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+    return sock;
 }
