8c305721e63eed6c7850db62b5ac3e19cdcbabe0
Mason <ab300819@163.com>
2022-03-21 17:21:35 +0800

    feat: 添加 cmocka 单元测试

---
+++ src/core_in_c/test_cmocka.c
@@ -0,0 +1,19 @@
+//
+// Created by mason on 2022/3/21.
+//
+
+#include <cmocka.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+/* A test case that does nothing and succeeds. */
+static void null_test_success(void **state) {
+	(void)state; /* unused */
+}
+int main(void) {
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(null_test_success),
+	};
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
