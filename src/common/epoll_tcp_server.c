73a452a45a35a160ae6b8c83aa48af9fccaade8f
Mason <ab300819@hotmail.com>
2022-11-27 23:01:02 +0800

    add epoll demo

---
+++ src/common/epoll_tcp_server.c
@@ -0,0 +1,169 @@
+//
+// Created by mason on 22-11-9.
+//
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define MAX_CONNECT 9
+#define MAX_EVENT 32
+#define BUF_SIZE 16
+#define MAX_LINE 256
+
+void server_run();
+void client_run();
+
+int main(int argc, char *argv[]) {
+  int opt;
+  char role = 's';
+  while ((opt = getopt(argc, argv, "cs")) != -1) {
+    switch (opt) {
+      case 'c':
+        role = 'c';
+        break;
+      case 's':
+        break;
+      default:
+        printf("usage: %s [-cs]\n", argv[0]);
+        exit(1);
+    }
+  }
+
+  if (role == 's') {
+    server_run();
+  } else {
+    client_run();
+  }
+
+  return EXIT_SUCCESS;
+}
+
+static void epoll_ctl_add(int epfd, int fd, uint32_t events) {
+  struct epoll_event ev;
+  ev.events = events;
+  ev.data.fd = fd;
+
+  if (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev) == -1) {
+    perror("epoll_ctl()\n");
+    exit(1);
+  }
+}
+
+static void set_sockaddr(struct sockaddr_in *addr) {
+  bzero((char *)addr, sizeof(struct sockaddr_in));
+  addr->sin_family = AF_INET;
+  addr->sin_addr.s_addr = INADDR_ANY;
+  addr->sin_port = htons(PORT);
+}
+
+static int set_nonblocking(int sock_fd) {
+  if (fcntl(sock_fd, F_SETFD, fcntl(sock_fd, F_GETFD, 0) | O_NONBLOCK) == -1) {
+    return -1;
+  }
+  return 0;
+}
+
+void server_run() {
+  int n;
+  int epfd;
+  int nfds;
+  int listen_sock;
+  int conn_sock;
+  int sock_len;
+  char buf[BUF_SIZE];
+  struct sockaddr_in srv_addr;
+  struct sockaddr_in cli_addr;
+  struct epoll_event events[MAX_EVENT];
+
+  listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  set_sockaddr(&srv_addr);
+  bind(listen_sock, (struct sockaddr *)&srv_addr, sizeof(srv_addr));
+
+  set_nonblocking(listen_sock);
+  listen(listen_sock, MAX_CONNECT);
+
+  epfd = epoll_create(1);
+  epoll_ctl_add(epfd, listen_sock, EPOLLIN | EPOLLOUT | EPOLLET);
+
+  sock_len = sizeof(cli_addr);
+  for (;;) {
+    nfds = epoll_wait(epfd, events, MAX_EVENT, -1);
+    for (int i = 0; i < nfds; ++i) {
+      if (events[i].data.fd == listen_sock) {
+        conn_sock =
+            accept(listen_sock, (struct sockaddr *)&cli_addr, &sock_len);
+        inet_ntop(AF_INET, (char *)&(cli_addr.sin_addr),buf, sizeof(cli_addr));
+        printf("[+] connected with %s:%d\n", buf, ntohs(cli_addr.sin_port));
+        set_nonblocking(conn_sock);
+        epoll_ctl_add(epfd, conn_sock,
+                      EPOLLIN | EPOLLET | EPOLLRDHUP | EPOLLHUP);
+      } else if (events[i].events & EPOLLIN) {
+        for (;;) {
+          bzero(buf, sizeof(buf));
+          n = read(events[i].data.fd, buf, sizeof(buf));
+          if (n <= 0) {
+            break;
+          } else {
+            printf("[+]data: %s\n", buf);
+            write(events[i].data.fd, buf, strlen(buf));
+          }
+        }
+      } else {
+        printf("[+] unexpected\n");
+      }
+      if (events[i].events & (EPOLLRDHUP | EPOLLHUP)) {
+        printf("[+] connection closed\n");
+        epoll_ctl(epfd, EPOLL_CTL_DEL, events[i].data.fd, NULL);
+        close(events[i].data.fd);
+        continue;
+      }
+    }
+  }
+}
+
+void client_run() {
+  int n;
+  int c;
+  int sock_fd;
+  char buf[MAX_LINE];
+  struct sockaddr_in srv_addr;
+
+  sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  set_sockaddr(&srv_addr);
+
+  if (connect(sock_fd, (struct sockaddr *)&srv_addr, sizeof(srv_addr)) < 0) {
+    perror("connect()");
+    exit(1);
+  }
+
+  for (;;) {
+    printf("input: ");
+    fgets(buf, sizeof(buf), stdin);
+    c = strlen(buf) - 1;
+    buf[c] = '\0';
+    write(sock_fd, buf, c + 1);
+
+    bzero(buf, sizeof(buf));
+    while (errno != EAGAIN && (n = read(sock_fd, buf, sizeof(buf))) > 0) {
+      printf("echo: %s\n", buf);
+      bzero(buf, sizeof(buf));
+
+      c -= n;
+      if (c <= 0) {
+        break;
+      }
+    }
+  }
+  close(sock_fd);
+}
