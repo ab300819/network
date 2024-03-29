9b938eb4fbe714bece29b690d3dd33b502648eb3
Mason <ab300819@hotmail.com>
2024-03-30 01:05:24 +0800

    添加《网络编程实战》期中作业客户端

--- src/networking_in_action/middle_client.c
+++ src/networking_in_action/middle_client.c
@@ -8,16 +8,41 @@ int main(int argc, char **argv) {
     char recv_line[MAX_LINE], send_line[MAX_LINE];
     memset(send_line, 0, MAX_LINE);
 
-    while (fgets(send_line, MAXLINE, stdin) != NULL) {
-        int n_bytes = sizeof(send_line);
-        if (write(sock_fd, send_line, n_bytes) != n_bytes) {
-            error(1, errno, "write error");
+    fd_set read_fd;
+    int max_fdp = sock_fd + 1;
+
+    while (1) {
+        FD_ZERO(&read_fd);
+        FD_SET(fileno(stdin), &read_fd);
+        FD_SET(sock_fd, &read_fd);
+
+        if (select(max_fdp, &read_fd, NULL, NULL, NULL) == -1) {
+            perror("select error");
+            exit(1);
         }
-        if (read(sock_fd, recv_line, MAX_LINE) == 0) {
-            error(1, errno, "server terminated prematurely");
+
+        if (FD_ISSET(sock_fd, &read_fd)) { // socket is readable
+            int n = read(sock_fd, recv_line, MAXLINE);
+            if (n < 0) {
+                perror("read error");
+                exit(1);
+            } else if (n == 0) { // server closed connection
+                printf("Server closed the connection.\n");
+                exit(0);
+            }
+            recv_line[n] = 0; // null terminate
+            fputs(recv_line, stdout);
         }
-        fputs(recv_line, stdout);
+
+        if (FD_ISSET(fileno(stdin), &read_fd)) { // input is readable
+            if (fgets(send_line, MAXLINE, stdin) == NULL) break; // EOF
+            if (send(sock_fd, send_line, strlen(send_line), 0) < 0) {
+                perror("send error");
+                exit(1);
+            }
+        }
     }
 
+    close(sock_fd);
     return EXIT_SUCCESS;
 }
